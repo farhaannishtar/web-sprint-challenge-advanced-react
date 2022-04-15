246c218b9fab7ac95d3e32302d01f120e9edd29d
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-04-15 12:10:59 -0400

    Able to move across board

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -6,13 +6,94 @@ export default class AppClass extends React.Component {
     coordinateX: 2,
     coordinateY: 2,
     totalMoves: 0,
-    board: ["", "", "", 
-            "", "B", "",       // ["", "", "", "", "", "", "", "", ""]
-            "", "", ""]
+    board: ["", "", "", "", "", "", "", "", ""],
+    message: ""
   }
 
   handleUp = () => {
-    console.log("We can do this");
+    console.log("We are going up");
+    if (this.state.coordinateY === 1) {
+      this.setState({
+        message: "You can't go up"
+      })
+    } else {
+     this.setState({
+        ... this.state,
+       coordinateY: this.state.coordinateY - 1
+      });
+    }
+  }
+
+  handleDown = () => {
+    console.log("We are going down");
+    if (this.state.coordinateY === 3) {
+      this.setState({
+        message: "You can't go down"
+      })
+    } else {
+     this.setState({
+        ... this.state,
+       coordinateY: this.state.coordinateY + 1
+      });
+    }
+  }
+
+  handleRight = () => {
+    console.log("We are going right");
+    if (this.state.coordinateX === 3) {
+      this.setState({
+        message: "You can't go right"
+      })
+    } else {
+     this.setState({
+        ... this.state,
+       coordinateX: this.state.coordinateX + 1
+      });
+    }
+  }
+
+  handleLeft = () => {
+    console.log("We are going left");
+    if (this.state.coordinateX === 1) {
+      this.setState({
+        message: "You can't go left"
+      })
+    } else {
+     this.setState({
+        ... this.state,
+       coordinateX: this.state.coordinateX - 1
+      });
+    }
+  }
+
+  mapCoordinatesWithPosition = (index) => {
+    if (this.state.coordinateX === 1 && this.state.coordinateY === 1 && index === 0) {
+      return true;
+    }
+    if (this.state.coordinateX === 2 && this.state.coordinateY === 1 && index === 1) {
+      return true;
+    } 
+    if (this.state.coordinateX === 3 && this.state.coordinateY === 1 && index === 2) {
+      return true;
+    }
+    if (this.state.coordinateX === 1 && this.state.coordinateY === 2 && index === 3) {
+      return true;
+    }
+    if (this.state.coordinateX === 2 && this.state.coordinateY === 2 && index === 4) {
+      return true;
+    } 
+    if (this.state.coordinateX === 3 && this.state.coordinateY === 2 && index === 5) {
+      return true;
+    }
+    if (this.state.coordinateX === 1 && this.state.coordinateY === 3 && index === 6) {
+      return true;
+    }
+    if (this.state.coordinateX === 2 && this.state.coordinateY === 3 && index === 7) {
+      return true;
+    }
+    if (this.state.coordinateX === 3 && this.state.coordinateY === 3 && index === 8) {
+      return true;
+    }
   }
 
   render() {
@@ -25,6 +106,15 @@ export default class AppClass extends React.Component {
         </div>
         <div id="grid">
           {this.state.board.map((value, index) => {
+            // If the value matches with the coordinates, 
+            // Set orange square for the div
+            if (this.mapCoordinatesWithPosition(index) === true) {
+              return ( 
+                <div key={index} className="square active">
+                  B
+                </div>
+              )
+            }
             return ( 
               <div key={index} className="square">
                 {value}
@@ -33,13 +123,13 @@ export default class AppClass extends React.Component {
           })}
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
+          <button id="left" onClick={this.handleLeft}>LEFT</button>
           <button id="up" onClick={this.handleUp}>UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
+          <button id="right" onClick={this.handleRight}>RIGHT</button>
+          <button id="down" onClick={this.handleDown}>DOWN</button>
           <button id="reset">reset</button>
         </div>
         <form>
