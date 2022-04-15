91b6fa8a44f7ad3a5c4944aaea759e1eb243108f
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-04-15 10:23:50 -0400

    Initialized state on Class Component

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,31 +1,43 @@
 import React from 'react'
 
 export default class AppClass extends React.Component {
+
+  state = {
+    coordinateX: 2,
+    coordinateY: 2,
+    totalMoves: 0,
+    board: ["", "", "", 
+            "", "B", "",       // ["", "", "", "", "", "", "", "", ""]
+            "", "", ""]
+  }
+
+  handleUp = () => {
+    console.log("We can do this");
+  }
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.coordinateX}, {this.state.coordinateY})</h3>
+          <h3 id="steps">You moved {this.state.totalMoves} times</h3>
         </div>
         <div id="grid">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square active">B</div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+          {this.state.board.map((value, index) => {
+            return ( 
+              <div key={index} className="square">
+                {value}
+              </div>
+            )
+          })}
         </div>
         <div className="info">
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
           <button id="left">LEFT</button>
-          <button id="up">UP</button>
+          <button id="up" onClick={this.handleUp}>UP</button>
           <button id="right">RIGHT</button>
           <button id="down">DOWN</button>
           <button id="reset">reset</button>
